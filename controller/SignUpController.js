b8f14cb27989906cf0ef381b71c1f60e707393df
Ayusha200333 <ayushawijerathna@gmail.com>
2025-05-20 00:53:52 +0530

    edit signup controler

--- controller/SignUpController.js
+++ controller/SignUpController.js
@@ -45,4 +45,9 @@ $("#signUp_signUp").on('click',function () {
             $('#loginPage').css("display","block");
         }
     })
+
+    function validatePassword(password) {
+        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+        return regex.test(password);
+    }
 })
